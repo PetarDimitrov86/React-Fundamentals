04f96c89c9684506c39f87e5ef4e7c2fd46a3134
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-18 11:22:00 +0200

    Introduce basic state setup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import Intro from './components/Intro'
+import Intro from '../Intro'
 import './App.css';
 
 class App extends Component {
+  state = {
+    series: []
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +15,7 @@ class App extends Component {
             <h3>TV Series List</h3>
           </p>
           <Intro message="Here you can find all of your most loved series" />
+          The lenght of series array - {this.state.series.length}
         </header>
       </div>
     );
